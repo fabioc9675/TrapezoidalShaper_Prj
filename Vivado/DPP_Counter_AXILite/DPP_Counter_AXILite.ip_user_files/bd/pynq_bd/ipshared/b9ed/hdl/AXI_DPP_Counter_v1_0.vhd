b5e02e94a5442f351073de6b13f03e9fb0ade1b0
fabioc9675 <fabian.castano@udea.edu.co>
2024-06-10 23:02:57 -0500

    Creation a copy of projecto to add AXI Lite support and AXI Stream in ADC

---
+++ Vivado/DPP_Counter_AXILite/DPP_Counter_AXILite.ip_user_files/bd/pynq_bd/ipshared/b9ed/hdl/AXI_DPP_Counter_v1_0.vhd
@@ -0,0 +1,98 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity AXI_DPP_Counter_v1_0 is
+	generic (
+		-- Users to add parameters here
+
+		-- User parameters ends
+		-- Do not modify the parameters beyond this line
+
+
+		-- Parameters of Axi Master Bus Interface M00_AXIS
+		C_M00_AXIS_TDATA_WIDTH	: integer	:= 32;
+		C_M00_AXIS_START_COUNT	: integer	:= 32
+	);
+	port (
+		-- Users to add ports here
+		--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+        dpp_clk    :  in  std_logic;
+        dpp_rst    :  in  std_logic;
+        -- ap_start   :  in  std_logic;
+        dpp_in     :  in  std_logic_vector (31 downto 0);
+        dpp_out    :  out std_logic_vector (31 downto 0);
+        -- fifo_out   :  out std_logic_vector (31 downto 0);
+        --%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+		-- User ports ends
+		-- Do not modify the ports beyond this line
+
+
+		-- Ports of Axi Master Bus Interface M00_AXIS
+		m00_axis_aclk	: in std_logic;
+		m00_axis_aresetn	: in std_logic;
+		m00_axis_tvalid	: out std_logic;
+		m00_axis_tdata	: out std_logic_vector(C_M00_AXIS_TDATA_WIDTH-1 downto 0);
+		m00_axis_tstrb	: out std_logic_vector((C_M00_AXIS_TDATA_WIDTH/8)-1 downto 0);
+		m00_axis_tlast	: out std_logic;
+		m00_axis_tready	: in std_logic
+	);
+end AXI_DPP_Counter_v1_0;
+
+architecture arch_imp of AXI_DPP_Counter_v1_0 is
+
+	-- component declaration
+	component AXI_DPP_Counter_v1_0_M00_AXIS is
+		generic (
+		C_M_AXIS_TDATA_WIDTH	: integer	:= 32;
+		C_M_START_COUNT	: integer	:= 32
+		);
+		port (
+		--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+        dpp_clk    :  in  std_logic;
+        dpp_rst    :  in  std_logic;
+        -- ap_start   :  in  std_logic;
+        dpp_in     :  in  std_logic_vector (31 downto 0);
+        dpp_out    :  out std_logic_vector (31 downto 0);
+        -- fifo_out   :  out std_logic_vector (31 downto 0);
+        --%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+		M_AXIS_ACLK	: in std_logic;
+		M_AXIS_ARESETN	: in std_logic;
+		M_AXIS_TVALID	: out std_logic;
+		M_AXIS_TDATA	: out std_logic_vector(C_M_AXIS_TDATA_WIDTH-1 downto 0);
+		M_AXIS_TSTRB	: out std_logic_vector((C_M_AXIS_TDATA_WIDTH/8)-1 downto 0);
+		M_AXIS_TLAST	: out std_logic;
+		M_AXIS_TREADY	: in std_logic
+		);
+	end component AXI_DPP_Counter_v1_0_M00_AXIS;
+
+begin
+
+-- Instantiation of Axi Bus Interface M00_AXIS
+AXI_DPP_Counter_v1_0_M00_AXIS_inst : AXI_DPP_Counter_v1_0_M00_AXIS
+	generic map (
+		C_M_AXIS_TDATA_WIDTH	=> C_M00_AXIS_TDATA_WIDTH,
+		C_M_START_COUNT	=> C_M00_AXIS_START_COUNT
+	)
+	port map (
+	    --%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+        dpp_clk         => dpp_clk,
+        dpp_rst         => dpp_rst,
+        dpp_in          => dpp_in,
+        dpp_out         => dpp_out,
+        -- fifo_out        => fifo_out,
+        --%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+		M_AXIS_ACLK	=> m00_axis_aclk,
+		M_AXIS_ARESETN	=> m00_axis_aresetn,
+		M_AXIS_TVALID	=> m00_axis_tvalid,
+		M_AXIS_TDATA	=> m00_axis_tdata,
+		M_AXIS_TSTRB	=> m00_axis_tstrb,
+		M_AXIS_TLAST	=> m00_axis_tlast,
+		M_AXIS_TREADY	=> m00_axis_tready
+	);
+
+	-- Add user logic here
+
+	-- User logic ends
+
+end arch_imp;
